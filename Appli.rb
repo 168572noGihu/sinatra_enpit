f7aa52df3e407ac8bf698893f22a6492bea60b63
68572noGihu <k168572@ie.u-ryukyu.ac.jp>
2016-09-19 07:14:13 +0000

    3

--- Appli.rb
+++ Appli.rb
@@ -1,10 +1,19 @@
-# myapp.rb
+# -*- coding: utf-8 -*-
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
-end
 
-get '?/:id' do
-  'Hello world!'
+<<EOS
+ 
+  あなたは遺跡に冒険に来ています。
+  各階毎に3つの選択肢から1つを選び、より多くのお金を稼ぎましょう。
+  しかし死んでしまった場合、今までのお金は0になってしまいます。
+
+  <p>
+  <input type="button" value="危険な道を進む" onclick="あなたは死んでしまった!">
+  <input type="button" value="安全な道を探す" onclick="あなたは死んでしまった!">
+  <input type="button" value="引き返す" onclick="あなたは無事に帰れなかった!">
+  </p>
+EOS
+
 end
